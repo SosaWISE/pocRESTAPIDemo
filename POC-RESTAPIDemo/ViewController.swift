8efde9772ba83264895c9939358cb7936d8256f1
Andres Sosa <asosa@fluenthome.com>
2018-12-11 10:57:21 -0700

    Adding all files

---
+++ POC-RESTAPIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  POC-RESTAPIDemo
+//
+//  Created by Andres Sosa on 12/11/18.
+//  Copyright © 2018 Andres Sosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
